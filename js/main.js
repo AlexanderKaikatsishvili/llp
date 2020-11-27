6a13cd7adb2a50a3f04634286dd10a3eb14788b5
Alexander Kaikatsishvili <alexanderkaikatsishvili@gmail.com>
2020-11-27 11:03:58 +0200

    Отключение для мобильных устройств параллакса

--- js/main.js
+++ js/main.js
@@ -168,73 +168,80 @@ AOS.init({
 
 /* GSUP SMOOTH PAGE SCROLL */
 
-let html = document.documentElement;
-let body = document.body;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $(".content-flow").removeClass("content-flow");
+    $(".content-container").removeClass("content-container");
+} else {
+    let html = document.documentElement;
+    let body = document.body;
 
-let scroller = {
-    target: document.querySelector(".content-container"),
-    ease: 0.07, // <= scroll speed
-    endY: 0,
-    y: 0,
-    resizeRequest: 1,
-    scrollRequest: 0,
-};
+    let scroller = {
+        target: document.querySelector(".content-container"),
+        ease: 0.07, // <= scroll speed
+        endY: 0,
+        y: 0,
+        resizeRequest: 1,
+        scrollRequest: 0,
+    };
 
-let requestId = null;
-
-TweenLite.set(scroller.target, {
-    force3D: true
-});
-
-window.addEventListener("load", onLoad);
-
-function onLoad() {
-    updateScroller();
-    window.focus();
-    window.addEventListener("resize", onResize);
-    document.addEventListener("scroll", onScroll);
-}
-
-function updateScroller() {
-
-    let resized = scroller.resizeRequest > 0;
-
-    if (resized) {
-        let height = scroller.target.clientHeight;
-        body.style.height = height + "px";
-        scroller.resizeRequest = 0;
-    }
-
-    let scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
-    scroller.endY = scrollY;
-    scroller.y += (scrollY - scroller.y) * scroller.ease;
-
-    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-        scroller.y = scrollY;
-        scroller.scrollRequest = 0;
-    }
+    let requestId = null;
 
     TweenLite.set(scroller.target, {
-        y: -scroller.y
+        force3D: true
     });
 
-    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
+    window.addEventListener("load", onLoad);
 
-function onScroll() {
-    scroller.scrollRequest++;
-    if (!requestId) {
-        requestId = requestAnimationFrame(updateScroller);
+    function onLoad() {
+        updateScroller();
+        window.focus();
+        window.addEventListener("resize", onResize);
+        document.addEventListener("scroll", onScroll);
+    }
+
+    function updateScroller() {
+
+        let resized = scroller.resizeRequest > 0;
+
+        if (resized) {
+            let height = scroller.target.clientHeight;
+            body.style.height = height + "px";
+            scroller.resizeRequest = 0;
+        }
+
+        let scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+        scroller.endY = scrollY;
+        scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+        if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+            scroller.y = scrollY;
+            scroller.scrollRequest = 0;
+        }
+
+        TweenLite.set(scroller.target, {
+            y: -scroller.y
+        });
+
+        requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+    }
+
+    function onScroll() {
+        scroller.scrollRequest++;
+        if (!requestId) {
+            requestId = requestAnimationFrame(updateScroller);
+        }
+    }
+
+    function onResize() {
+        scroller.resizeRequest++;
+        if (!requestId) {
+            requestId = requestAnimationFrame(updateScroller);
+        }
     }
 }
 
-function onResize() {
-    scroller.resizeRequest++;
-    if (!requestId) {
-        requestId = requestAnimationFrame(updateScroller);
-    }
-}
+
 
 
 /* YOUTUBE FRAME INITIALISATION */
