f7eb5b399691abdcf55e63d348a26867791114df
Alexander Kaikatsishvili <alexanderkaikatsishvili@gmail.com>
2020-12-07 13:47:57 +0200

    Вернул анимацию для мобильных

--- js/main.js
+++ js/main.js
@@ -191,8 +191,8 @@ $(".menu-list__link").on('click', function(){
 /* GSUP SMOOTH PAGE SCROLL */
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $(".grid--dark").addClass("grid--dark-mobile");
-    $("[data-aos^=fade][data-aos^=fade]").css("opacity", "1");
+    // $(".grid--dark").addClass("grid--dark-mobile");
+    // $("[data-aos^=fade][data-aos^=fade]").css("opacity", "1");
 
     $('.footer').css('position', 'initial');
 
@@ -233,77 +233,76 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         footerHeight = footer.outerHeight();
         contentWrapper.css("margin-bottom", footerHeight);
     }
-
-
-    (function() {
-
-        const container = document.getElementById('app');
-
-        const options = {
-            onSpeedUp: (ev) => {
-            },
-            onSlowDown: (ev) => {
-            },
-            // mountainDistortion || LongRaceDistortion || xyDistortion || turbulentDistortion || turbulentDistortionStill || deepDistortionStill || deepDistortion
-            distortion: LongRaceDistortion,
-
-            length: 400,
-            roadWidth: 10,
-            islandWidth: 5,
-            lanesPerRoad: 2,
-
-            fov: 90,
-            fovSpeedUp: 150,
-            speedUp: 2,
-            carLightsFade: 0.4,
-
-            totalSideLightSticks: 50,
-            lightPairsPerRoadWay: 70,
-
-            // Percentage of the lane's width
-            shoulderLinesWidthPercentage: 0.05,
-            brokenLinesWidthPercentage: 0.1,
-            brokenLinesLengthPercentage: 0.5,
-
-            /*** These ones have to be arrays of [min,max].  ***/
-            lightStickWidth: [0.12, 0.5],
-            lightStickHeight: [1.3, 1.7],
-
-            movingAwaySpeed: [60, 80],
-            movingCloserSpeed: [-120, -160],
-
-            /****  Anything below can be either a number or an array of [min,max] ****/
-
-            // Length of the lights. Best to be less than total length
-            carLightsLength: [400 * 0.05, 400 * 0.15],
-            // Radius of the tubes
-            carLightsRadius: [0.05, 0.14],
-            // Width is percentage of a lane. Numbers from 0 to 1
-            carWidthPercentage: [0.3, 0.5],
-            // How drunk the driver is.
-            // carWidthPercentage's max + carShiftX's max -> Cannot go over 1.
-            // Or cars start going into other lanes
-            carShiftX: [-0.2, 0.2],
-            // Self Explanatory
-            carFloorSeparation: [0.05, 1],
-
-            colors: {
-                roadColor: 0x080808,
-                islandColor: 0x0a0a0a,
-                backgroundCanvas: 0x111111,
-                shoulderLines: 0x131318,
-                brokenLines: 0x131318,
-                /***  Only these colors can be an array ***/
-                leftCars: [0xFF5F73, 0xE74D60, 0xff102a],
-                rightCars: [0xA4E3E6, 0x80D1D4, 0x53C2C6],
-                sticks: 0xA4E3E6,
-            }
-        };
-
-        const myApp = new App(container, options);
-        myApp.loadAssets().then(myApp.init)
-    })()
 }
+
+(function() {
+
+    const container = document.getElementById('app');
+
+    const options = {
+        onSpeedUp: (ev) => {
+        },
+        onSlowDown: (ev) => {
+        },
+        // mountainDistortion || LongRaceDistortion || xyDistortion || turbulentDistortion || turbulentDistortionStill || deepDistortionStill || deepDistortion
+        distortion: LongRaceDistortion,
+
+        length: 400,
+        roadWidth: 10,
+        islandWidth: 5,
+        lanesPerRoad: 2,
+
+        fov: 90,
+        fovSpeedUp: 150,
+        speedUp: 2,
+        carLightsFade: 0.4,
+
+        totalSideLightSticks: 50,
+        lightPairsPerRoadWay: 70,
+
+        // Percentage of the lane's width
+        shoulderLinesWidthPercentage: 0.05,
+        brokenLinesWidthPercentage: 0.1,
+        brokenLinesLengthPercentage: 0.5,
+
+        /*** These ones have to be arrays of [min,max].  ***/
+        lightStickWidth: [0.12, 0.5],
+        lightStickHeight: [1.3, 1.7],
+
+        movingAwaySpeed: [60, 80],
+        movingCloserSpeed: [-120, -160],
+
+        /****  Anything below can be either a number or an array of [min,max] ****/
+
+        // Length of the lights. Best to be less than total length
+        carLightsLength: [400 * 0.05, 400 * 0.15],
+        // Radius of the tubes
+        carLightsRadius: [0.05, 0.14],
+        // Width is percentage of a lane. Numbers from 0 to 1
+        carWidthPercentage: [0.3, 0.5],
+        // How drunk the driver is.
+        // carWidthPercentage's max + carShiftX's max -> Cannot go over 1.
+        // Or cars start going into other lanes
+        carShiftX: [-0.2, 0.2],
+        // Self Explanatory
+        carFloorSeparation: [0.05, 1],
+
+        colors: {
+            roadColor: 0x080808,
+            islandColor: 0x0a0a0a,
+            backgroundCanvas: 0x111111,
+            shoulderLines: 0x131318,
+            brokenLines: 0x131318,
+            /***  Only these colors can be an array ***/
+            leftCars: [0xFF5F73, 0xE74D60, 0xff102a],
+            rightCars: [0xA4E3E6, 0x80D1D4, 0x53C2C6],
+            sticks: 0xA4E3E6,
+        }
+    };
+
+    const myApp = new App(container, options);
+    myApp.loadAssets().then(myApp.init)
+})()
 
 
 
